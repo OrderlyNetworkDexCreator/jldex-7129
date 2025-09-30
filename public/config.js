d0754da9add29ad99e837cb08e7dbc7189ca3eae
Mario Reder <mario.reder@pm.me>
2025-09-30 03:34:49 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Portfolio,Trading,Markets,Rewards,Leaderboard,Vaults",
-  "VITE_CUSTOM_MENUS": "",
+  "VITE_CUSTOM_MENUS": "$JL, https://pump.fun/coin/6bQbWiGDGfrdjNG3FBS5i3yFjpQQKroZCSBWtVkWpump",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://t.me/jewliquid",
   "VITE_DISCORD_URL": "",
